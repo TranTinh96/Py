ea8e681a3b61ce11789bef341039ddc6a106466c
Tran Tinh <tranngoctinh96@gmail.com>
2019-12-15 21:22:32 +0700

    update

--- App/App/forms.py
+++ App/App/forms.py
@@ -4,3 +4,11 @@ class ContactHome(forms.Form):
    full_name = forms.CharField()
    email = forms.EmailField()
    content = forms.CharField(widget=forms.Textarea)
+
+   # Validate Data on Fields
+   def clean_email(self,*args, **kwargs):
+      email = self.cleaned_data.get('email')
+      print(email)
+      if email.endswith(".edu"):
+         raise forms.ValidationError("This is not a valid email . Please don't not .edu")
+      return email
--- App/Blog/forms.py
+++ App/Blog/forms.py
@@ -7,6 +7,14 @@ class BlogPostForm(forms.Form):
     content = forms.CharField(widget=forms.Textarea)
 
 class BlogPostModelForm(forms.ModelForm):
-   class Meta :
-      model = BlogPost
-      fields = ['title','content']
+    class Meta :
+        model = BlogPost
+        fields = ['title','content']
+
+         # Validate Data on Fields
+        def clean_title(self,*args, **kwargs):
+            title = self.cleaned_data.get('title')
+            qs = BlogPost.objects.filter(title=title)
+            if qs.exists():
+                raise forms.ValidationError(" This is title has already been use.Please try again ! ")
+            return title
